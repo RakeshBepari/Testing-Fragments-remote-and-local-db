47e74f68e1a6cce844adce58f60385d4a9fe4ec3
jha1Raghu <jha1raghu@gmail.com>
2021-08-27 19:26:15 +0530

    no main dispatcher for coroutines in test directory error : Fixed

--- app/src/test/java/com/example/learningtesting/ui/ShoppingViewModelTest.kt
+++ app/src/test/java/com/example/learningtesting/ui/ShoppingViewModelTest.kt
@@ -1,20 +1,26 @@
 package com.example.learningtesting.ui
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.learningtesting.MainCoroutineRule
 import com.example.learningtesting.other.Constants.MAX_NAME_LENGTH
 import com.example.learningtesting.other.Constants.MAX_PRICE_LENGTH
 import com.example.learningtesting.other.Status
 import com.example.learningtesting.repositories.FakeShoppingRepository
 import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
 
+@ExperimentalCoroutinesApi
 class ShoppingViewModelTest {
 
     @get:Rule
     var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
 
     private lateinit var shoppingViewModel: ShoppingViewModel
 
@@ -90,6 +96,15 @@ class ShoppingViewModelTest {
 
         val currImageUrl = shoppingViewModel.curImageUrl.getOrAwaitValueTest()
         assertThat(currImageUrl).isEmpty()
+    }
+
+    @Test
+    fun `fun setCurImageUrl sets `(){
+        shoppingViewModel.setCurImageUrl("url")
+
+        val value = shoppingViewModel.curImageUrl.getOrAwaitValueTest()
+
+        assertThat(value).isEqualTo("url")
     }
 
 }
